0180188a08520c93598fde8516e8d8b50f494bf5
pablo <pablo@finotebook-linux>
2016-09-12 08:17:53 -0300

    Syncronynizing project

--- src/main/java/com/utn/progav2/App.java
+++ src/main/java/com/utn/progav2/App.java
@@ -1,5 +1,12 @@
 package com.utn.progav2;
 
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import com.utn.progav2.entities.Persona;
+import com.utn.progav2.util.HibernateUtil;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+   
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		Persona p = new Persona();
+		p.setNombre("Pablo");
+		p.setApellido("Fino");
+		p.setEdad(25);
+		p.setFechaNacimiento(new Date());
+		session.save(p);
+		session.getTransaction().commit();
+		
     }
 }
