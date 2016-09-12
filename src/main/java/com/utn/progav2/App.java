25a0985b13421f56e0861fcfc938ee84c6256b42
pfino <pablo.fino@globant.com>
2016-09-12 15:23:12 -0300

    Changes for travis

--- src/main/java/com/utn/progav2/App.java
+++ src/main/java/com/utn/progav2/App.java
@@ -11,21 +11,20 @@ import com.utn.progav2.util.HibernateUtil;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-   
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		
-		Persona p = new Persona();
-		p.setNombre("Pablo");
-		p.setApellido("Fino");
-		p.setEdad(25);
-		p.setFechaNacimiento(new Date());
-		session.save(p);
-		session.getTransaction().commit();
-		
+		    Session session = HibernateUtil.getSessionFactory().openSession();
+		    session.beginTransaction();
+
+    		Persona p = new Persona();
+    		p.setNombre("Pablo");
+    		p.setApellido("Fino");
+    		p.setEdad(25);
+    		p.setFechaNacimiento(new Date());
+    		session.save(p);
+    		session.getTransaction().commit();
+
     }
 }
